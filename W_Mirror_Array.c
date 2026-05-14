589998d4d0e025a3e879a34501b59c9444f8696d
Satyajit Ghosh <satyajit_jbl@yahoo.com>
2026-05-14 16:00:32 +0600

    problem solution Tmatrix added

---
+++ W_Mirror_Array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+     int r , c;
+     scanf("%d %d, &r &c");
+     printf("%d", r);
+    //  for (int i = 0; i < r; i++)
+    //  {
+    //     for (int j = 0; j < c; j++)
+    //     {
+    //         scanf("%d, &a[i]a[j]");
+    //     }
+        
+    //  }
+    //  for (int i = 0; i < r; i++)
+    //  {
+    //     for (int j = 0; j < c; j++)
+    //     {
+    //         printf("%d, a[i]a[j]");
+    //     }
+        
+    //  }
+     
+return 0;
+}
